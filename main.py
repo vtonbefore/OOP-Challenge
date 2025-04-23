07432ba00a03db2366c4dad3a26dbb8c86ed200b
marion <beforevton@gmail.com>
2025-04-23 17:17:14 +0300

    Final version working - pet class assignment

---
+++ main.py
@@ -0,0 +1,17 @@
+from pet import Pet
+
+# Create a pet
+my_pet = Pet("Max")
+
+# Test methods
+my_pet.get_status()
+my_pet.eat()
+my_pet.sleep()
+my_pet.play()
+my_pet.get_status()
+
+# Bonus
+my_pet.train("sit")
+my_pet.train("roll over")
+my_pet.show_tricks()
+
